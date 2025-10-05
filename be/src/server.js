d04547ea821b0089424abd989df5f1a341ffa9d7
baole405 <baolttse184569@fpt.edu.vn>
2025-10-05 15:45:15 +0700

    feat: git ignore update

--- be/src/server.js
+++ be/src/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import notesRoutes from "./routes/notes.routes.js";
 import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 
-app.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
